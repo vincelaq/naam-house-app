c64756b120cfa4f4a52b0217405d437611ae9589
Vincent Laquindanum <laq.vince@gmail.com>
2021-12-13 06:27:47 -0800

    setup project

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import Header from './components/Header'
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Header text='Is person of interest a veteran?' />
+        <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60,
   },
-});
+})
+
+export default App
---
+++ components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import AppLoading from 'expo-app-loading'
+import { useFonts, Inter_600SemiBold } from '@expo-google-fonts/inter'
+
+const Header = ({ text }) => {
+    let [fontsLoaded] = useFonts({
+        Inter_600SemiBold,
+    });
+    
+
+    if (!fontsLoaded) {
+        return <AppLoading />
+    } else {
+        return (
+            <View style={styles.header}>
+                <Text style={styles.text}>{text}</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    header: {
+        height: 60,
+        padding: 15,
+    },
+    text: {
+        color: '#000',
+        fontFamily: 'Inter_600SemiBold',
+        fontSize: 30,
+        textAlign: 'center'
+    }
+})
+
+export default Header
